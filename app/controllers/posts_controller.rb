932c44ddb44151fd2ef748cedac37830688ecbd0
erikn775 <erikn775@gmail.com>
2021-01-14 22:00:22 -0700

    added new post form and started to add methods to post controller

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,2 +1,36 @@
 class PostsController < ApplicationController
+
+    def index
+
+    end
+
+    def new
+
+    end
+
+    def create
+
+    end
+
+    def show
+
+    end
+
+    def edit
+
+    end
+
+    def update
+
+    end
+
+    def destroy
+
+    end
+
+    private
+
+    def post_params
+        params(:post).permit(:image_url, :title, :content, :category_id)
+    end
 end
